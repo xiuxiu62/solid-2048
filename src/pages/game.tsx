2c2fef7d8473379a6567959bc5b821aba18f0803
jacremer <jacremer@live.com>
2021-09-18 00:21:45 -0700

    clean up

---
+++ src/pages/game.tsx
@@ -0,0 +1,7 @@
+import { JSXElement } from 'solid-js';
+
+export default (): JSXElement => (
+	<section class="m-10">
+		<header class="text-3xl text-center bg-white">Game</header>
+	</section>
+);
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
 export { default as Home } from './home';
-export { default as Game } from './Game';
+export { default as Game } from './game';
 export { default as Scores } from './scores';
 export { default as NotFound } from './not-found';
